55c63fc755cb4dab20119d5f14f1afeb6556023d
Luís Guilherme Tavares da Silva Martorano <luismartorano@gmail.com>
2021-08-18 22:47:18 -0300

    FInal

--- main.tf
+++ main.tf
@@ -6,19 +6,12 @@ provider "aws" {
 }
 //state é armazenado dentro do s3 da Amazon.  
 terraform {
-  
-  #backend "s3" {
-    # Lembre de trocar o bucket para o seu, não pode ser o mesmo nome
-    #bucket = "terraform-luismartorano"
-    #dynamodb_table = "terraform-state-lock-dynamo"            //state locking
-    #key    = "terraform-princ.tfstate"
-    #region = "us-east-2"
-  #}
   backend "s3" {
-    organization = "MARTORANODEVOPS"
-
-    workspaces {
-      name = "martorano"
-    }
+     Lembre de trocar o bucket para o seu, não pode ser o mesmo nome
+    bucket = "terraform-luismartorano"
+    #dynamodb_table = "terraform-state-lock-dynamo"            //state locking
+    key    = "terraform-princ.tfstate"
+    region = "us-east-2"
   }
+  
 }
